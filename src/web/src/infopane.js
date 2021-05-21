98034385836f69fe1c6343ff4043fcd7f3ab4968
Jacob Allen <enoua5@gmail.com>
2021-05-21 09:00:13 -0600

    move resizeTextToLine toutil.js

--- src/web/src/infopane.js
+++ src/web/src/infopane.js
@@ -41,32 +41,6 @@ function showPieceInfo(pid, captures)
   resizeTextToLine(l("piece-name"), name, 0.9, 30);
 }
 
-function resizeTextToLine(text, value, factor, max_iter)
-{
-  // reset the font size so we don't get fucky nonsense frame-to-frame
-  let default_fontsize = getDefaultProperty(text.tagName, "fontSize");
-  let fontsize = parseFloat(default_fontsize);
-  text.style.fontSize = default_fontsize;
-  // some fiddling to get the text the right size
-  // for loop instead of while, to avoid hanging
-  for(let i = 0; i < max_iter; i++)
-  {
-    text.innerText = "*";
-    let natural_height = text.getBoundingClientRect().height;
-    
-    text.innerText = value;
-    let current_height = text.getBoundingClientRect().height;
-    
-    // text is is one line, we break here
-    if(current_height <= natural_height)
-      break;
-      
-    // if the text is taking up two lines...
-    fontsize *= factor;
-    text.style.fontSize = fontsize+"px";
-  }
-}
-
 function clearPieceInfo()
 {
   l("piece-name").innerText = "";
--- src/web/src/util.js
+++ src/web/src/util.js
@@ -6,6 +6,32 @@ function indexOfElement(el)
   return i;
 }
 
+function resizeTextToLine(text, value, factor, max_iter)
+{
+  // reset the font size so we don't get fucky nonsense frame-to-frame
+  let default_fontsize = getDefaultProperty(text.tagName, "fontSize");
+  let fontsize = parseFloat(default_fontsize);
+  text.style.fontSize = default_fontsize;
+  // some fiddling to get the text the right size
+  // for loop instead of while, to avoid hanging
+  for(let i = 0; i < max_iter; i++)
+  {
+    text.innerText = "*";
+    let natural_height = text.getBoundingClientRect().height;
+    
+    text.innerText = value;
+    let current_height = text.getBoundingClientRect().height;
+    
+    // text is is one line, we break here
+    if(current_height <= natural_height)
+      break;
+      
+    // if the text is taking up two lines...
+    fontsize *= factor;
+    text.style.fontSize = fontsize+"px";
+  }
+}
+
 // https://stackoverflow.com/a/51597423/3154580
 function getDefaultProperty(tag, prop)
 {
